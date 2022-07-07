7af3686f78106b041cc64d9508a0bf7f6b3c5501
Efe YILDIRIM <vakeliefe@hotmail.com.tr>
2022-07-07 09:37:31 +0300

    V0.1

---
+++ Hollie/Application/Concrete/Country.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Concrete
+{
+    public class Country
+    {
+        [Key]
+        [Column(TypeName = "nvarchar")]
+        [StringLength(20)]
+        public string Code { get; set; }
+        [Column(TypeName = "nvarchar")]
+        [StringLength(50)]
+        public string Name { get; set; }
+    }
+}
