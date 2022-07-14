2d9e36b484f15588aff84e6b4a3cab451fcd0929
damlaozdemir2k <ozdemirdamla2000@gmail.com>
2022-07-14 16:10:07 +0300

    market

---
+++ api/Controllers/MarketController.cs
@@ -0,0 +1,19 @@
+﻿using Application.Concrete;
+using Application.Dtos;
+using Application.Infrastructure;
+using DataAccess.Concrate;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarketController : ControllerBase
+    {
+    }
+}
