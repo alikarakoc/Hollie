5ed9ee1cf8c4b74e50460977e8cce8dd4bccd770
Sıla Soyat <sila.soyat10@gmail.com>
2022-07-14 16:06:43 +0300

    beggining of board controller

---
+++ Application/Dtos/GetAllBoardDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class GetAllBoardDto
+    {
+        public int Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string CreatedUser { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public string UpdateUser { get; set; }
+    }
+}
---
+++ api/Controllers/BoardController.cs
@@ -0,0 +1,37 @@
+﻿using Application.Concrete;
+using Application.Dtos;
+using Application.Infrastructure;
+using DataAccess.Concrate;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardController : Controller
+    {
+ 
+        Context _context = new Context();
+
+        [HttpGet]
+        [Route("AllBoard")]
+
+        public ActionResponse<List<Board>> GetAllBoards()
+        {
+            ActionResponse<List<Board>> actionResponse = new()
+            {
+                ResponseType = ResponseType.Ok,
+                IsSuccessful = true,    
+            };
+
+            return actionResponse;
+        }
+        
+    }
+}
