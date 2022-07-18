1709cb26fcadff48e3c7c56108ea6fcdc2ffb82b
Sıla Soyat <sila.soyat10@gmail.com>
2022-07-18 08:46:21 +0300

    update countryController

--- api/Controllers/CountryController.cs
+++ api/Controllers/CountryController.cs
@@ -7,20 +7,8 @@ namespace api.Controllers
     [ApiController]
     public class CountryController : ControllerBase
     {
-        Context _context = new Context();
 
-        [HttpGet]
-        [Route("AllBoard")]
 
-        public ActionResponse<List<Board>> GetAllBoards()
-        {
-            ActionResponse<List<Board>> actionResponse = new()
-            {
-                ResponseType = ResponseType.Ok,
-                IsSuccessful = true,
-            };
-
-            return actionResponse;
-        }
+       
     }
 }
