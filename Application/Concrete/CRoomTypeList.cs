e876664d65fead2b2db999bfb8e9e33979b55565
damlaozdemir2k <ozdemirdamla2000@gmail.com>
2022-07-26 14:27:00 +0300

    Update RoomTypeList

---
+++ Application/Concrete/CRoomTypeList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Application.Concrete
+{
+    public class CRoomTypeList
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int RoomTypeId { get; set; }
+
+        public int ListId { get; set; }
+
+
+
+    }
+}
---
+++ Application/Dtos/CRoomTypeDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class CRoomTypeDto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int RoomTypeId { get; set; }
+
+        public string ListId { get; set; }
+    }
+}
