5157905769079554d333c71e2dd95b4f2fa55428
Sıla Soyat <sila.soyat10@gmail.com>
2022-07-28 14:59:05 +0300

    adding cboardcontroller

--- Application/Concrete/Contract.cs
+++ Application/Concrete/Contract.cs
@@ -37,5 +37,6 @@ namespace Application.Concrete
 
 
 
+
     }
 }
---
+++ api/Controllers/CBoardController.cs
@@ -0,0 +1,43 @@
+﻿using Application.Concrete;
+using Application.Infrastructure;
+using DataAccess.Concrate;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace api.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CBoardController : Controller
+    {
+        private readonly Context _context;
+        public CBoardController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("AllCBoard")]
+
+        public ActionResponse<List<CBoardList>> GetAllCBoards()
+        {
+            ActionResponse<List<CBoardList>> actionResponse = new()
+            {
+                ResponseType = ResponseType.Ok,
+                IsSuccessful = true,
+            };
+            var cBoardList = _context.CBoards;
+            if (cBoardList != null&& cBoardList.Count()>0)
+            {
+                actionResponse.Data = cBoardList.ToList();
+            }
+            return actionResponse;
+        }
+    }
+}
