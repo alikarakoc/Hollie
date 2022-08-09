c78f7d7a691c4993aa73734d3392babd440c3bda
Sıla Soyat <sila.soyat10@gmail.com>
2022-08-09 22:48:17 +0300

    devam edecek...

---
+++ DataAccess/Repository/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using Application.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class UserRepository
+    {
+        public List<UserLogin> TestUsers;
+        public UserRepository()
+        {
+            TestUsers = new List<UserLogin>();
+            TestUsers.Add(new UserLogin() { UserName = "User1", Password  = "Pass1" });
+            TestUsers.Add(new UserLogin() { UserName = "User2", Password = "Pass2" });
+        }
+        public UserLogin GetUser(string username)
+        {
+            try
+            {
+                return TestUsers.First(user => user.UserName.Equals(username));
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Domain/Concrate/TokenManager.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+//namespace Domain.Concrate
+//{
+
+//    public static string GenerateToken(string userName)
+//    {
+
+//        byte[] key = Convert.FromBase64String(Secret);
+//        SymmetricSecurityKey securityKey = new SymmetricSecurityKey(key);
+//        SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor
+//        {
+//            Subject = new ClaimsIdentity(new[] {
+//                      new Claim(ClaimTypes.Name, userName)}),
+//            Expires = DateTime.UtcNow.AddMinutes(30),
+//            SigningCredentials = new SigningCredentials(securityKey,
+//            SecurityAlgorithms.HmacSha256Signature)
+//        };
+
+//        JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+//        JwtSecurityToken token = handler.CreateJwtSecurityToken(descriptor);
+//        return handler.WriteToken(token);
+//    }
+}
